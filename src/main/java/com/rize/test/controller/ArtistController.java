686e26868efd859aa19bd5b3651a4bdce5fd6024
Muhammad Saqib Chaudhry <young_innovator@yahoo.com>
2022-02-27 11:37:40 +0100

    improvement to removes usage of repository directly from controller layer

--- src/main/java/com/rize/test/controller/ArtistController.java
+++ src/main/java/com/rize/test/controller/ArtistController.java
@@ -1,8 +1,6 @@
 package com.rize.test.controller;
 
-import com.rize.test.exception.ArtistNotFoundException;
 import com.rize.test.model.Artist;
-import com.rize.test.respository.ArtistRepository;
 import com.rize.test.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +12,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/artists")
@@ -22,44 +19,33 @@ import java.util.Optional;
 public class ArtistController {
 
     @Autowired
-    private ArtistRepository artistRepository;
-
-    @Autowired
     private ArtistService artistService;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> show(@PathVariable("id") Integer id) {
-        return ResponseEntity.ok(findArtist(id));
+        return ResponseEntity.ok(artistService.findArtistById(id));
     }
 
     @GetMapping
     public ResponseEntity<List<Artist>> index(@RequestParam(value = "category", required = false) String category,
-                                                 @RequestParam(value = "search", required = false) String search,
-                                                 @RequestParam(value = "birthday-month", required = false)
-                                                     @Min (value = 1, message = "birthday-month must be greater than or equal to 1")
-                                                     @Max (value = 12, message = "birthday-month must be less than or equal to 12")
-                                                             Integer birthdayMonth) {
+                                              @RequestParam(value = "search", required = false) String search,
+                                              @RequestParam(value = "birthday-month", required = false)
+                                              @Min(value = 1, message = "birthday-month must be greater than or equal to 1")
+                                              @Max(value = 12, message = "birthday-month must be less than or equal to 12")
+                                                      Integer birthdayMonth) {
         return ResponseEntity.ok(artistService.findArtists(category, search, birthdayMonth));
     }
 
     @PostMapping
     public ResponseEntity<Artist> create(@Valid @RequestBody Artist artist) {
-        artistRepository.save(artist);
-        return ResponseEntity.created(URI.create("/artist/"+artist.getId())).body(artist);
+        artistService.saveArtist(artist);
+        return ResponseEntity.created(URI.create("/artist/" + artist.getId())).body(artist);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
-        Artist artist = findArtist(id);
-        artistRepository.delete(artist);
+        Artist artist = artistService.findArtistById(id);
+        artistService.deleteArtist(artist);
         return ResponseEntity.ok(artist);
-    }
-
-    private Artist findArtist(Integer id){
-        Optional<Artist> artist = artistRepository.findById(id);
-        if(artist.isEmpty()){
-            throw new ArtistNotFoundException(id);
-        }
-        return artist.get();
     }
 }
--- src/main/java/com/rize/test/service/ArtistService.java
+++ src/main/java/com/rize/test/service/ArtistService.java
@@ -1,5 +1,6 @@
 package com.rize.test.service;
 
+import com.rize.test.exception.ArtistNotFoundException;
 import com.rize.test.model.Artist;
 import com.rize.test.respository.ArtistRepository;
 import com.rize.test.specification.ArtistSpecificationBuilder;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.util.StringUtils.hasLength;
 
@@ -17,8 +19,24 @@ public class ArtistService {
     @Autowired
     private ArtistRepository artistRepository;
 
+    public Artist findArtistById(Integer id) {
+        Optional<Artist> artist = artistRepository.findById(id);
+        if (artist.isEmpty()) {
+            throw new ArtistNotFoundException(id);
+        }
+        return artist.get();
+    }
+
+    public void saveArtist(Artist artist) {
+        artistRepository.save(artist);
+    }
+
+    public void deleteArtist(Artist artist) {
+        artistRepository.delete(artist);
+    }
+
     public List<Artist> findArtists(String category, String search, Integer birthdayMonth) {
-        if(isFilter(category, search, birthdayMonth)){
+        if (isFilter(category, search, birthdayMonth)) {
             Specification<Artist> artistSpecification = new ArtistSpecificationBuilder()
                     .withCategory(category)
                     .withSearch(search)
